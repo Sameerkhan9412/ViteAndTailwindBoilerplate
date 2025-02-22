f5c26321f79666cd2c486656b2f2d6c3083aca8c
sameerkhan9412 <sameerkhann9412@gmail.com>
2025-02-22 15:20:47 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black text-red-500'>
+      HELLO DUNIA
+    </div>
+  )
+}
+
+export default App
